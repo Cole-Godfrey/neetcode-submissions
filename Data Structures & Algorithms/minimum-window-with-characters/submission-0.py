51e396d1928e845689dc348671d08f6fb69bc7a1
Cole Godfrey <colegodfrey34@gmail.com>
2026-09-09 18:09:11 -0700

    Add: minimum-window-with-characters - submission-0

---
+++ Data Structures & Algorithms/minimum-window-with-characters/submission-0.py
@@ -0,0 +1,38 @@
+class Solution:
+    def minWindow(self, s: str, t: str) -> str:
+        if t == "": return ""
+        tCounter = Counter(t)
+        l = 0
+        res = []
+        resLen = float('infinity')
+        need = len(tCounter)
+        have = 0
+        window = defaultdict(int)
+
+        for r in range(len(s)):
+            # add r to window
+            char = s[r]
+            window[char] += 1
+            if char in tCounter and tCounter[char] == window[char]:
+                have += 1
+            newLen = r - l + 1    
+            if have >= need and newLen < resLen:
+                # update new best substr
+                res = [l, r + 1]
+                resLen = newLen
+            # increment l until no longer good
+            while have >= need:
+                newLen = r - l + 1
+                if have >= need and newLen < resLen:
+                    # update new best substr
+                    res = [l, r + 1]
+                    resLen = newLen
+                char = s[l]
+                window[char] -= 1
+                if char in tCounter and window[char] == tCounter[char] - 1:
+                    have -= 1
+                l += 1
+        return s[res[0]:res[1]] if res else ""
+            
+            
+            
